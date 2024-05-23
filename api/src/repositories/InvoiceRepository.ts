a67d72ebef777c5051067312bedf7337239267b7
felipehenrique159 <felipecod6@hotmail.com>
2024-05-23 01:21:00 -0300

    feat: adicionado interface na criação de nova fatura

--- api/src/repositories/InvoiceRepository.ts
+++ api/src/repositories/InvoiceRepository.ts
@@ -1,15 +1,16 @@
 import Invoice from "../database/models/Invoice";
+import { PdfExtractData } from "../interfaces/PdfExtractData";
 
 export default class InvoiceRepository {
-    static async create(idCustomer: number, extractedData: any) {
+    static async create(idCustomer: number, extractedData: PdfExtractData) {
         await Invoice.create({
             filename: extractedData.filename,
             path: 'invoices/' + extractedData.filename,
             instalation_number: extractedData.instalationNumber,
             month_reference: extractedData.monthReference,
-            electric_energy_quant: extractedData.electricEnergy.quant,
-            electric_energy_united_value: extractedData.electricEnergy.unitedValue,
-            electric_energy_value: extractedData.electricEnergy.value,
+            electric_energy_quant: extractedData.electricEnergy ? extractedData.electricEnergy.quant : null,
+            electric_energy_united_value: extractedData.electricEnergy ? extractedData.electricEnergy.unitedValue : null,
+            electric_energy_value: extractedData.electricEnergy ? extractedData.electricEnergy.value : null,
             energy_scee_icms_quant: extractedData.energySceeIcms ? extractedData.energySceeIcms.quant : null,
             energy_scee_icms_united_value: extractedData.energySceeIcms ? extractedData.energySceeIcms.unitedValue : null,
             energy_scee_icms_value: extractedData.energySceeIcms ? extractedData.energySceeIcms.value : null,
